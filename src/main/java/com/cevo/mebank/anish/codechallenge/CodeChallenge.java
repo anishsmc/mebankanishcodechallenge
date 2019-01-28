c092828fa566700462c438717bd66bc1a89db208
Anish Mohile <amohile@whispir.com>
2019-01-28 19:23:10 +1100

    User Input Logic

--- src/main/java/com/cevo/mebank/anish/codechallenge/CodeChallenge.java
+++ src/main/java/com/cevo/mebank/anish/codechallenge/CodeChallenge.java
@@ -1,22 +1,38 @@
 package com.cevo.mebank.anish.codechallenge;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Stream;
 
 public class CodeChallenge {
 
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String args[]) {
 
-        String fileName = "path-to-file/filename.csv";
+        String fileName = args[0];//"path-to-file/filename.csv";
 
 
-        // Obtain the File
-        // Create a Transaction Structure
-        // Call the calculator
-        // done??
+        Stream<String> streamedFile;
+        try {
+            streamedFile = getStreamfromFile(fileName);
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+            System.exit(1);
+        }
+
+        List<Transaction> transactionList = transformStreamToTransactions(streamedFile);
+        RelativeBalanceCalculator rbc = new RelativeBalanceCalculator(transactionList);
+
+        System.out.println(calculateAndGetOutputOnUserInput(rbc));
+
+
+        // recursively call until exit
 
 
 
@@ -29,17 +45,27 @@ public class CodeChallenge {
 
     }
 
-    private Stream<String> getStreamfromFile(String fileNameWithPath) throws java.io.IOException {
+    private static Stream<String> getStreamfromFile(String fileNameWithPath) throws java.io.IOException {
         Stream<String> transactions = Files.lines(Paths.get(fileNameWithPath));
         return transactions;
     }
 
-    private List<Transaction> transformStreamToTransactions(Stream<String> transactions) {
+    private static List<Transaction> transformStreamToTransactions(Stream<String> transactions) {
         List<Transaction> transactionList = new ArrayList<>();
         transactions.forEach(transaction -> {
             Transaction t = new Transaction(transaction.split("/,/"));
             transactionList.add(t);
         });
         return transactionList;
+    }
+
+    private static String calculateAndGetOutputOnUserInput(RelativeBalanceCalculator rbc){
+        System.out.println("Enter the From Date for Balance Calculation: ");
+        LocalDateTime fromDate = LocalDateTime.parse(scanner.next(), GlobalConstants.formatter);
+        System.out.println("Enter the To Date for Balance Calculation: ");
+        LocalDateTime toDate = LocalDateTime.parse(scanner.next(), GlobalConstants.formatter);
+        System.out.println("Enter the Account ID for Balance Calculation: ");
+        String accountID = scanner.next();
+        return rbc.calculateRelativeBalanceFor(accountID, fromDate, toDate);
     }
 }
